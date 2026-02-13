8beabb2a8faf3d6568b8eb04e018f06b4960fd7d
Suleman <sp@digitalgraphiks.ae>
2026-02-13 15:42:03 +0500

    activity log

--- app/Http/Controllers/API/ActivityLogController.php
+++ app/Http/Controllers/API/ActivityLogController.php
@@ -3,25 +3,35 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Database\QueryException;
-use Symfony\Component\HttpFoundation\Response;
-use Mail;
-use Auth;
-use Session;
-use DB;
-use Illuminate\Support\Facades\Validator;
-use Carbon\Carbon; 
-use Illuminate\Support\Str;
-use Spatie\Activitylog\Models\Activity;
+use App\Models\Menu;
 use App\Models\User;
+use App\Services\PermissionService;
+use Carbon\Carbon; 
+use DB;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Mail;
+use Session;
+use Spatie\Activitylog\Models\Activity;
+use Symfony\Component\HttpFoundation\Response;
 
 class ActivityLogController extends Controller
 {
+    protected $permissionService;
+
+    public function __construct(PermissionService $permissionService)
+    {
+        $this->permissionService = $permissionService;
+    }
     
     public function get_all_activity(Request $request){
-   
+        $authUser = Auth::user();
+        $menuUuid = request()->header('menu-uuid'); 
+        $permissions = $this->permissionService->checkPermissions($menuUuid); 
         // $get_all_activity = Activity::orderBy('created_at', 'desc')->join('users', 'activity_log.causer_id', '=', 'users.id')
         // ->select('activity_log.*', 'users.first_name as causer_name')
         // ->paginate(12);
@@ -32,6 +42,21 @@ class ActivityLogController extends Controller
         // ], Response::HTTP_OK);
         // Columns for sorting (update according to your columns)
         $columns = ['id', 'description', 'causer_name', 'created_at'];
+
+        // DataTables pagination parameters
+        // $limit = $request->input('length', 12); // Default to 12 if not provided
+        // $start = $request->input('start', 0);
+
+        $perPage = $request->get('per_page', 10); // default items per page
+        $search = $request->get('search'); // search keyword
+        // $orderColumn = $columns[$request->input('order.0.column', 0)];
+        // $searchValue = $request->input('search.value', '');
+       
+        // Query with join, search, and order
+        // $query = Activity::orderBy('created_at', 'desc')
+        //     ->join('users', 'activity_log.causer_id', '=', 'users.id')
+        //     ->select('activity_log.*', 'users.first_name as causer_name');
+
 
         // DataTables pagination parameters
         $limit = $request->input('length', 12); // Default to 12 if not provided
@@ -58,24 +83,78 @@ class ActivityLogController extends Controller
         // Apply pagination
       
         $data = $query->offset($start)
-                    ->limit($limit)
-                    ->get();
-        // Format response for DataTables
-        $json_data = [
-            "draw" => intval($request->input('draw')),
-            "recordsTotal" => intval($totalData),
-            "recordsFiltered" => intval($totalFiltered),
-            "data" => $data
-        ];
+                    ->limit($limit);
+                    // ->get();
+                    
+        // if ($permissions['view']) {
+        //     if (!$permissions['viewglobal']) {
+        //         $data = $data->where('activity_log.causer_id', $authUser->id)->get();
+        //     }
+        // }else{
+        //     if (Auth::user()->hasPermission('viewglobal')) {
+        //         $data = $data->get();
+        //     } else {
+        //         return response()->json(['message' => 'You do not have permission to view this menu'], Response::HTTP_FORBIDDEN);
+        //     }
+        // }
+         if ($permissions['viewglobal']) {
+            $data = $data->get();
+
+        } elseif ($permissions['view']) {
+            $data = $data->where('activity_log.causer_id', $authUser->id)->get();
+
+        } else {
+            return response()->json(
+                ['message' => 'You do not have permission to view this menu'],
+                Response::HTTP_FORBIDDEN
+            );
+        }
+           
+        // if (!empty($searchValue)) {
+        //     $query->where(function($q) use ($searchValue) {
+        //         $q->where('log_name', 'LIKE', "%{$searchValue}%")
+        //           ->orWhere('users.first_name', 'LIKE', "%{$searchValue}%")
+        //           ->orWhere('event', 'LIKE', "%{$searchValue}%");
+        //     });
+        //     }
+
+             if (!empty($search)) {
+            $query->where(function ($q) use ($search) {
+                $q->where('log_name', 'LIKE', "%{$search}%")
+                  ->orWhere('users.first_name', 'LIKE', "%{$search}%")
+                  ->orWhere('event', 'LIKE', "%{$search}%");
+            });
+        }
+
+        // $totalData = Activity::count(); // Total records count
+        // $totalFiltered = $query->count(); // Filtered records count
+
+        // // Apply pagination
+      
+        // $data = $query->offset($start)
+        //             ->limit($limit)
+        //             ->get();
+        // // Format response for DataTables
+        // $json_data = [
+        //     "draw" => intval($request->input('draw')),
+        //     "recordsTotal" => intval($totalData),
+        //     "recordsFiltered" => intval($totalFiltered),
+        //     "data" => $data
+        // ];
+        $activities = $query->paginate($perPage);
+            $data = $activities;
         return response()->json([
             'status_code' => Response::HTTP_OK,
-            'data' => $json_data,
+            'data' => $data,
         ], Response::HTTP_OK);
     
     }
 
     public function get_active_activity(Request $request){
-   
+        $menuUuid = Menu::where('url', 'activitylog')->first()->uuid;
+        $authUser = Auth::user();
+        // $menuUuid = request()->header('menu-uuid'); 
+        $permissions = $this->permissionService->checkPermissions($menuUuid); 
         // $get_all_activity = Activity::orderBy('created_at', 'desc')->join('users', 'activity_log.causer_id', '=', 'users.id')
         // ->select('activity_log.*', 'users.first_name as causer_name')
         // ->paginate(12);
@@ -85,6 +164,7 @@ class ActivityLogController extends Controller
         //     'data' => $get_all_activity,
         // ], Response::HTTP_OK);
         // Columns for sorting (update according to your columns)
+        
         $columns = ['id', 'description', 'causer_name', 'created_at'];
 
         // DataTables pagination parameters
@@ -112,8 +192,33 @@ class ActivityLogController extends Controller
         // Apply pagination
       
         $data = $query->offset($start)
-                    ->limit($limit)
-                    ->get();
+                    ->limit($limit);
+                    // ->get();
+    
+        // if ($permissions['view']) {
+        //     if (!$permissions['viewglobal']) {
+        //         $data = $data->where('activity_log.causer_id', $authUser->id)->get();
+        //     }
+        // }else{
+        //     if (Auth::user()->hasPermission('viewglobal')) {
+        //         $data = $data->get();
+        //     } else {
+        //         return response()->json(['message' => 'You do not have permission to view this menu'], Response::HTTP_FORBIDDEN);
+        //     }
+        // }  
+        if ($permissions['viewglobal']) {
+            $data = $data->get();
+
+        } elseif ($permissions['view']) {
+            $data = $data->where('activity_log.causer_id', $authUser->id)->get();
+
+        }
+        //  else {
+        //     return response()->json(
+        //         ['message' => 'You do not have permission to view this menu'],
+        //         Response::HTTP_FORBIDDEN
+        //     );
+        // }            
         // Format response for DataTables
         $json_data = [
             "draw" => intval($request->input('draw')),
@@ -124,6 +229,8 @@ class ActivityLogController extends Controller
         return response()->json([
             'status_code' => Response::HTTP_OK,
             'data' => $json_data,
+            'menu_uuid' => $menuUuid,
+            'permissions' => $permissions
         ], Response::HTTP_OK);
     
     }
